c3199e8cc3b8b94481373e8727fa77dfd5812a67
F219258AdeelTahir <f219258@cfd.nu.edu.pk>
2022-11-26 12:18:52 +0500

    Add project files.

---
+++ Data Structure Project Text Editor/TrieTree.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+class TriTree {
+	struct Node {
+		char data;
+		bool colour;
+		Node* childeren[26]={NULL};
+		Node()
+		{
+			data = '-';
+			colour = false;
+		}
+	};
+	ifstream read;
+	Node* root;
+public:
+	TriTree()
+	{
+		root = new Node;
+		string line;
+		read.open("Dictionary.txt");
+		while (getline(read, line)) {
+			insert(root,line);
+		}
+		read.close();
+		Display(root);
+	}
+	void insert(Node*& node, string str, int length = 0)
+	{
+		if (length < str.length())
+		{
+			for (int i = 0; i < 26 ; i++)
+			{
+				if (node->childeren[i])
+				{
+					if (node->childeren[i]->data == str[length])
+					{
+						if (length == str.length() - 1)
+						{
+							node->childeren[i]->colour = true;
+						}
+						insert(node->childeren[i], str, length + 1);
+						return;
+					}
+				}
+				else
+				{
+					node->childeren[i] = new Node;
+					node->childeren[i]->data = str[length];
+					if (length == str.length() - 1)
+					{
+						node->childeren[i]->colour = true;
+					}
+					insert(node->childeren[i], str, length + 1);
+					return;
+				}
+			}
+		}
+	}
+	void Display(Node*& node)
+	{
+		for (int i = 0; i < 26; i++)
+		{
+			if (node->childeren[i])
+			{
+				cout << node->childeren[i]->data<<" ";
+			}
+		}
+		for (int i = 0; i < 26; i++)
+		{
+			if (node->childeren[i])
+			{
+				Display(node->childeren[i]);
+			}
+		}
+		
+	}
+};
+int main()
+{
+	TriTree tree;
+
+}
