3c2323932efeb39036ad52f0114a96108601f8ea
M.Sufiyan <f219242@cfd.nu.edu.pk>
2022-12-05 00:54:34 +0500

    console created by sufiyan word are getting predicted

--- Data Structure Project Text Editor/main.cpp
+++ Data Structure Project Text Editor/main.cpp
@@ -1,12 +1,95 @@
-#include"TriTree.h"
+#include<iostream>
+#include<Windows.h>
+#include<conio.h>
+#include "TriTree.h"
+using namespace std;
 int main()
 {
-	//comment
-	TriTree tree;
-	string* arr = NULL;
-	int size = tree.Suggestion("app", arr);
-	for (int i = 0; i < size; i++)
+
+	//variables declartions
+	TriTree obj;
+	string *arr = NULL, str, str2;
+	string alpha = "";
+	char character = '/0';
+	int count = 0;
+	cout << "enter string" << endl;
+	while (alpha != ";")
 	{
-		cout << arr[i] << endl;
+		//using getch() function to read chracter
+	label:
+		if (count > 0)
+		{
+
+			cout << str2;
+		}
+		character = _getch();
+
+		if (character == ';')
+		{
+			cout << " " << endl;
+			break;
+		}
+
+		cout << character;
+
+		//concatenating with string
+		str += character;
+		str2 = str;
+		//checking if str.length()>=3 then it will show up the suggestions
+		if (str.length() >= 3 && !(str[0] == ' ' || str[1] == ' ' || str[2] == ' '))
+		{
+			int no_of_suggestions = obj.Suggestion(str, arr);
+			if (no_of_suggestions)
+			{
+
+				if (no_of_suggestions <= 10)
+				{
+					cout << "\t\t\t";
+					for (int i = 0;i < no_of_suggestions;i++)
+					{
+						//outputs those suggestion whose size is 8 or less
+						if (arr[i].length() <= 8)
+						{
+							cout << arr[i] << endl;
+							cout << "\t\t\t";
+							count++;
+						}
+					}
+					Sleep(10000);
+					system("cls");
+					goto label;
+				}
+				else
+				{
+
+					cout << "\t\t\t";
+					for (int i = 0;i < 10;i++)
+					{
+						//outputs those suggestion whose size is 8 or less
+						if (arr[i].length() <= 8)
+						{
+							cout << arr[i] << endl;
+							cout << "\t\t\t";
+							count++;
+						}
+					}
+					Sleep(10000);
+					system("cls");
+					goto label;
+
+				}
+			}
+
+			else
+			{
+				cout << "\nsorry but the given word has no suggestion" << endl;
+				return 0;
+			}
+
+		}
+
+
 	}
+	//system("pause");
+	return 0;
 }
